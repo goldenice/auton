106f19b8cafbcd3ae09c0fda5fa152252c37207c
Rick Lubbers <ik@ricklubbers.nl>
2016-05-01 01:05:37 +0200

    Add redux to the mix

--- server/api.js
+++ server/api.js
@@ -1,39 +1,51 @@
 'use strict';
 
 const socketio      = require('socket.io');
-const Container     = require('./container/container.js');
 const fs            = require('fs');
+const redux         = require('redux');
+const reducer       = require('./reducer.js');
+const nodeUuid      = require('node-uuid');
+
+const SAVEFILE = __dirname + '/data/state.json'
 
 module.exports = function attach(server) {
     const io = socketio(server);
-    let container;
-    try {
-        let data = JSON.parse(fs.readFileSync(__dirname + '/data/state.json'));
-        container = Container(data.state, data.hash);
-    } catch (e) {
-        container = Container({ todos: [] }, 0);
-    }
+    let uuid = null;
 
-    function save() {
-        fs.writeFile(__dirname + '/data/state.json', JSON.stringify({ state: container.getState(), hash: container.getHash() }), ()=>{});
-    }
+    // Set up redux store
+    let store;
+    (function() {
+        let initialState;
+        try {
+            let save = JSON.parse(fs.readFileSync(SAVEFILE));
+            uuid = save.uuid;
+            initialState = save.state;
+        } catch (e) {
+            initialState = require('./initialState.js');
+        }
+        store = redux.createStore(reducer, initialState);
+    })();
 
-    setInterval(save, 1000);
+    // Generate and push out new state on update
+    store.subscribe(() => {
+        uuid = nodeUuid.v4();
+        io.emit('state', store.getState(), uuid);
+    });
 
+    // Save app state on change
+    store.subscribe(() => {
+        fs.writeFile(SAVEFILE, JSON.stringify({ state: store.getState(), uuid: uuid }));
+    });
+
+    // When client connects
     io.on('connection', (socket) => {
-        console.log("Client connected");
-
         socket.on('state', function() {
-            socket.emit('state', container.getState(), container.getHash());
+            socket.emit('state', store.getState(), uuid);
         });
 
-        socket.on('action', (action, hash) => {
-            try {
-                io.emit('state', container.apply(action, hash), container.getHash());
-            } catch (err) {
-                socket.emit('response', err);
-            }
+        socket.on('action', (action, clientUuid) => {
+            if (uuid != clientUuid) return socket.emit('response', { err: 'invalid-update', state: store.getState() });
+            store.dispatch(action);
         });
-
     });
 }
--- server/container/tools/hashaction.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const sha1      = require('sha1');
-const hash      = require('./hashobject.js');
-
-module.exports = function(prevHash, action) {
-    return sha1(prevHash + hash(action));
-};
